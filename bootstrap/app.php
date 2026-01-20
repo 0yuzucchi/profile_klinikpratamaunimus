b425d0db19464b2887ac5badb4598ed9b83b116b
0yuzucchi <rsyddd2424@gmail.com>
2026-01-20 11:29:01 +0700

    finalisasi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         
         // Daftarkan alias di sini
+                $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => CheckRole::class,
         ]);
