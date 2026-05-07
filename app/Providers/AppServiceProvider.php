9b26782a25ec4584b8618254f72dfe73513cea10
0yuzucchi <rsyddd2424@gmail.com>
2026-05-07 14:13:41 +0700

    Coba Lagi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,15 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
         URL::forceScheme('https');
         }
+
+            if (config('app.env') === 'production' && isset($_ENV['VERCEL_JOB_ID'])) {
+        // Redirect folder cache dan view ke /tmp
+        $this->app->useStoragePath('/tmp/storage');
+        
+        // Buat folder jika belum ada (opsional, beberapa versi memerlukannya)
+        if (!is_dir('/tmp/storage/framework/views')) {
+            mkdir('/tmp/storage/framework/views', 0755, true);
+        }
+    }
     }
 }
